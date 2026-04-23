ce2ae0fc4f28997cc9d3108411bf80c203918395
RR <rrn091301@gmail.com>
2026-04-23 12:19:58 +0800

    refactor(guests): wire router to controller handlers
    Replace stub route callbacks with imported controller functions.

--- modules/bookings/routers/guestsRouter.js
+++ modules/bookings/routers/guestsRouter.js
@@ -1,18 +1,16 @@
 const { Router } = require('express');
+const {
+  getAllGuests,
+  createGuest,
+  getGuest,
+  updateGuest
+} = require('../controllers/guestsController');
 
 const guestsRouter = Router();
 
-guestsRouter.get('/', (req, res, next) => {
-  res.status(200).json({ info: 'Retrieved all guests' });
-});
-guestsRouter.post('/', (req, res, next) => {
-  res.status(201).json({ info: 'Created new guest' });
-});
-guestsRouter.get('/:id', (req, res, next) => {
-  res.status(200).json({ info: `Retrieved guest with id #${req.params.id}` });
-});
-guestsRouter.patch('/:id', (req, res, next) => {
-  res.status(200).json({ info: `Updated guest details with id #${req.params.id}` });
-});
+guestsRouter.get('/', getAllGuests);
+guestsRouter.post('/', createGuest);
+guestsRouter.get('/:id', getGuest);
+guestsRouter.patch('/:id', updateGuest);
 
 module.exports = guestsRouter;
