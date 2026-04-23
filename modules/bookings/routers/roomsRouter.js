463525eedeb32dcfade4be5bdcad2d5a678b27a9
RR <rrn091301@gmail.com>
2026-04-23 13:37:38 +0800

    style(rooms): remove accidental indentation in rooms router

--- modules/bookings/routers/roomsRouter.js
+++ modules/bookings/routers/roomsRouter.js
@@ -1,18 +1,18 @@
-  const { Router } = require('express');
-  const {
-    getAllRooms,
-    createRoom,
-    getRoom,
-    updateRoom,
-    deleteRoom
-  } = require('../controllers/roomsController.js');
+const { Router } = require('express');
+const {
+  getAllRooms,
+  createRoom,
+  getRoom,
+  updateRoom,
+  deleteRoom
+} = require('../controllers/roomsController.js');
 
-  const roomsRouter = Router();
+const roomsRouter = Router();
 
-  roomsRouter.get('/', getAllRooms);
-  roomsRouter.post('/', createRoom);
-  roomsRouter.get('/:id', getRoom);
-  roomsRouter.patch('/:id', updateRoom);
-  roomsRouter.delete('/:id', deleteRoom);
+roomsRouter.get('/', getAllRooms);
+roomsRouter.post('/', createRoom);
+roomsRouter.get('/:id', getRoom);
+roomsRouter.patch('/:id', updateRoom);
+roomsRouter.delete('/:id', deleteRoom);
 
-  module.exports = roomsRouter;
+module.exports = roomsRouter;
