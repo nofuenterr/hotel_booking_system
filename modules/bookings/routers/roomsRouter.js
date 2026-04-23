e338de031470df727503a2bed259731a176ac699
RR <rrn091301@gmail.com>
2026-04-23 13:29:29 +0800

    refactor(rooms): wire router to controller handlers
    Replace stub route callbacks with imported controller functions.

--- modules/bookings/routers/roomsRouter.js
+++ modules/bookings/routers/roomsRouter.js
@@ -1,21 +1,18 @@
-const { Router } = require('express');
+  const { Router } = require('express');
+  const {
+    getAllRooms,
+    createRoom,
+    getRoom,
+    updateRoom,
+    deleteRoom
+  } = require('../controllers/roomsController.js');
 
-const roomsRouter = Router();
+  const roomsRouter = Router();
 
-roomsRouter.get('/', (req, res, next) => {
-  res.status(200).json({ info: 'Retrieved all rooms' });
-});
-roomsRouter.post('/', (req, res, next) => {
-  res.status(201).json({ info: 'Created new room' });
-});
-roomsRouter.get('/:id', (req, res, next) => {
-  res.status(200).json({ info: `Retrieved room with id #${req.params.id}` });
-});
-roomsRouter.patch('/:id', (req, res, next) => {
-  res.status(200).json({ info: `Updated room details with id #${req.params.id}` });
-});
-roomsRouter.delete('/:id', (req, res, next) => {
-  res.status(200).json({ info: `Deleted room with id #${req.params.id}` });
-});
+  roomsRouter.get('/', getAllRooms);
+  roomsRouter.post('/', createRoom);
+  roomsRouter.get('/:id', getRoom);
+  roomsRouter.patch('/:id', updateRoom);
+  roomsRouter.delete('/:id', deleteRoom);
 
-module.exports = roomsRouter;
+  module.exports = roomsRouter;
