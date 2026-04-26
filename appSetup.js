c6fda2694afa2be024cdb79c876af3454bad60db
RR <rrn091301@gmail.com>
2026-04-26 16:35:07 +0800

    feat(app): serve static frontend files from public directory

--- appSetup.js
+++ appSetup.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.use('/', (req, res, next) => {
   console.log('Welcome to hotel booking system!');
