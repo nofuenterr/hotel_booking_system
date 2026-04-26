22569104acc5162eba521c38f4f24ce80695cc3d
RR <rrn091301@gmail.com>
2026-04-26 16:34:48 +0800

    fix(weather): add current temperature and weather code to forecast params

--- helpers/services/weatherService.js
+++ helpers/services/weatherService.js
@@ -6,6 +6,7 @@ const fetchWeatherForDate = async (date) => {
       params: {
         latitude: 51.5074,
 				longitude: -0.1278,
+        current: 'temperature_2m,weather_code',
         daily: 'temperature_2m_min,temperature_2m_max,weather_code',
         hourly: 'temperature_2m,weather_code',
         timezone: 'auto',
