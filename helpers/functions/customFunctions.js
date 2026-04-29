325c032c13fd495ea336ced0077f896bbfafa116
RR <rrn091301@gmail.com>
2026-04-29 16:33:04 +0800

    fix(helpers): add getRawCursorValue utility to normalize date values for cursor encoding

--- helpers/functions/customFunctions.js
+++ helpers/functions/customFunctions.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 const encodeCursor = (value, id, direction) => {
   return Buffer.from(JSON.stringify({ value, id, direction })).toString('base64');
 };
@@ -6,4 +8,13 @@ const decodeCursor = (cursor) => {
   return JSON.parse(Buffer.from(cursor, 'base64').toString('utf8'));
 };
 
-module.exports = { encodeCursor, decodeCursor };
+const getRawCursorValue = (item, column) => {
+  const value = item[column];
+  if (!value) return value;
+  if (column === 'check_in_date' || column === 'check_out_date') {
+    return moment(value).format('YYYY-MM-DD');
+  }
+  return value instanceof Date ? value.toISOString() : value;
+};
+
+module.exports = { encodeCursor, decodeCursor, getRawCursorValue };
